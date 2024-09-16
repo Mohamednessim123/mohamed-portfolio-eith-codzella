d58613a513fbd50f875406dccf665f42a555c4f9
Mohamednessim123 <mnessim620@gmail.com>
2024-09-16 12:08:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// window.onbeforeunload = function(){
+// localStorage.setItem("scroll",window.scrollY)
+// }
+// window.onafterload = function(){
+//     let scroll = localStorage.getItem("scroll")
+// }
+// if(scroll){
+// window.scrollTo(0,scroll)
+// }
